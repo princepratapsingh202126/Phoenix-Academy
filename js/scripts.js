91c1f70e636b9f107474d74dfa740ebee332a602
princepratapsingh202126 <73489380+princepratapsingh202126@users.noreply.github.com>
2024-08-18 19:48:20 +0530

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,35 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+<script src="https://cdn.emailjs.com/sdk/2.6.4/email.min.js"></script>
+
+<script>
+  (function() {
+    emailjs.init("YOUR_USER_ID");
+  })();
+
+  const form = document.getElementById('contactForm');
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const email = document.getElementById('email').value;
+    const templateId = 'template_fgfi4a3';
+    const serviceId = 'gmail';
+    const userId = 'Prince Pratap Singh';
+    const templateParams = {
+      from_name: 'Visitor',
+      to_name: 'Prince Pratap Singh',
+      reply_to: email,
+      message: 'Notify Me!'
+    };
+
+    emailjs.send(serviceId, templateId, templateParams, userId)
+      .then((response) => {
+        console.log('Email sent successfully!');
+      })
+      .catch((error) => {
+        console.error('Error sending email:', error);
+      });
+  });
+</script>
